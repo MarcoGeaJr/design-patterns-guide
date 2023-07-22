1524c5611cf4762e5989c7f3bfe6866aaa5fe216
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2023-07-22 12:46:15 -0300

    Abstract factory exmple

--- Creationals/AbstractFactory/Program.cs
+++ Creationals/AbstractFactory/Program.cs
@@ -6,12 +6,38 @@
  * Aqui é onde você pode aplicar o padrão "Abstract Factory" para resolver esse problema.
  */
 
+using AbstractFactory.Abstract;
+using AbstractFactory.Abstract.Products;
+using AbstractFactory.Factory;
+
 namespace AbstractFactory;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // concrect instance of abstract factory
+        IStoreAbstractFactory abstractFactory = new StoreAbstractFactory();
+
+        // Physical store product factory
+        IProductStoreFactory physicalStore = abstractFactory.CreatePhysicalStore();
+
+        // Online store product factory
+        IProductStoreFactory onlineStore = abstractFactory.CreateOnlineStore();
+
+        // Instace of physical product
+        IProduct pyhsicalProduct = physicalStore.CreateProduct("Physical Keyboard", 99.9M);
+
+        // Instace of online product
+        IProduct onlineProduct = onlineStore.CreateProduct("Online Keyboard", 99.9M);
+
+        // The ammount of discount must be applied for the both products
+        decimal commonDiscount = 10M;
+
+        Console.WriteLine($"Physical Product Real Price: US$ {pyhsicalProduct.GetPrice(0)}");
+        Console.WriteLine($"Physical Product Price: US$ {pyhsicalProduct.GetPrice(commonDiscount)}");
+        Console.WriteLine($"Online Product RealPrice: US$ {onlineProduct.GetPrice(0)}");
+        Console.WriteLine($"Online Product Price: US$ {onlineProduct.GetPrice(commonDiscount)}");
+        Console.ReadLine();
     }
 }
